cd58e444e616ee4ccea166d5cf01b33396b3b191
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-07-16 15:20:17 -0400

    Add futures for futureBuilder

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -11,6 +14,21 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -19,7 +37,7 @@ class DetailScreen extends StatelessWidget {
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(fontSize: 24),
         ),
       ),
@@ -31,7 +49,7 @@ class DetailScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Hero(
-              tag: id,
+              tag: widget.id,
               child: Container(
                 width: 250,
                 clipBehavior: Clip.hardEdge,
@@ -45,7 +63,7 @@ class DetailScreen extends StatelessWidget {
                   ],
                 ),
                 child: Image.network(
-                  thumb,
+                  widget.thumb,
                 ),
               ),
             ),
